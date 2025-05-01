195ff90659c5b0e79912b725d0b44519e7993372
EricSavagePersonal <eric.allan.savage@gmail.com>
2025-04-30 21:00:25 -0500

    Debug run_all.sh not executing main.py

--- backend/fetch_IB_margin_file.sh
+++ backend/fetch_IB_margin_file.sh
@@ -1,18 +1,17 @@
-#!/bin/bash -v
+#!/bin/bash
+set -e
 
-#output_dir="/Users/esavage/Library/Mobile Documents/com~apple~CloudDocs/T5_Related/IB_margin_files"
-output_dir="./IB_margin_files"
-#output_dir="/app/IB_margin_files"
+output_dir="/app/IB_margin_files"
 mkdir -p "$output_dir"
 
 file_path="$output_dir/$(date +%Y-%m-%d-%H-%M-%S)-margin_futures_fops.html"
 
-curl -o "$file_path" https://www.interactivebrokers.com/en/trading/margin-futures-fops.php || { echo "Download failed"; exit 1; }
+echo "Downloading to $file_path"
+curl -o "$file_path" https://www.interactivebrokers.com/en/trading/margin-futures-fops.php || {
+  echo "Download failed"
+  exit 1
+}
 
-# mac format sed invocation
-# sed -i '' 's/N\/A/0/g' "$file_path"
-
-# gnu flavored version
+echo "Download succeeded. Normalizing file..."
 sed -i 's/N\/A/0/g' "$file_path"
-
-grep 'N/A' "$file_path"
+grep 'N/A' "$file_path" || echo "No 'N/A' entries remaining"
