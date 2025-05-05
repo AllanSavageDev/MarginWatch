26e628c74f1ca075781ae2948f45ccdbb99dfa4e
EricSavagePersonal <eric.allan.savage@gmail.com>
2025-05-05 15:57:00 -0500

    Rename config.ini to config.prod.ini for environment separation

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,30 @@
 import configparser
 import os
 
+# def load_db_config():
+#     script_dir = os.path.dirname(os.path.realpath(__file__))
+#     config_path = os.path.join(script_dir, 'config.ini')
+
+#     config = configparser.ConfigParser()
+#     config.read(config_path)
+
+#     return {
+#         'host': config['postgres']['host'],
+#         'port': config['postgres']['port'],
+#         'user': config['postgres']['user'],
+#         'password': config['postgres']['password'],
+#         'database': config['postgres']['database']
+#     }
+
+
 def load_db_config():
     script_dir = os.path.dirname(os.path.realpath(__file__))
-    config_path = os.path.join(script_dir, 'config.ini')
+    
+    env = os.getenv('MW_ENV', 'dev')  # default to dev
+    filename = f'config.{env}.ini'    # e.g. config.dev.ini or config.prod.ini
 
+    config_path = os.path.join(script_dir, filename)
+    
     config = configparser.ConfigParser()
     config.read(config_path)
 
